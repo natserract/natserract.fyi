fd14007d3a8feca1976cd2da014165e5689af200
Alfin S <benjaminstwo@gmail.com>
2024-04-08 11:05:08 +0800

    feat: add mobile menu

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,6 +6,7 @@ import { useTheme } from ".";
 import { Icon } from "../util/icon";
 import { tinaField } from "tinacms/dist/react";
 import { GlobalHeader } from "../../tina/__generated__/types";
+import MobileMenu from "./mobile-menu";
 
 export const Header = ({ data }: { data: GlobalHeader }) => {
   const router = useRouter();
@@ -77,70 +78,77 @@ export const Header = ({ data }: { data: GlobalHeader }) => {
               </span>
             </Link>
           </h4>
-          <ul className="flex gap-6 sm:gap-8 lg:gap-10 tracking-[.002em] -mx-4">
-            {data.nav &&
-              data.nav.map((item, i) => {
-                const activeItem =
-                  (item.href === ""
-                    ? router.asPath === "/"
-                    : router.asPath.includes(item.href)) && isClient;
-                return (
-                  <li
-                    key={`${item.label}-${i}`}
-                    className={`${
-                      activeItem ? activeItemClasses[theme.color] : ""
-                    }`}
-                  >
-                    <Link
-                      data-tina-field={tinaField(item, "label")}
-                      href={`/${item.href}`}
-                      className={`relative select-none	text-base inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-8 px-4 ${
-                        activeItem ? `` : `opacity-70`
-                      }`}
-                    >
-                      {item.label}
-                      {activeItem && (
-                        <svg
-                          className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2 -z-1 opacity-10 dark:opacity-15 ${
-                            activeBackgroundClasses[theme.color]
+
+          <div className="navigation-menu">
+            <MobileMenu data={data} key={router.asPath} />
+
+            <div className="hidden sm:block">
+              <ul className="flex gap-6 sm:gap-8 lg:gap-10 tracking-[.002em] -mx-4">
+                {data.nav &&
+                  data.nav.map((item, i) => {
+                    const activeItem =
+                      (item.href === ""
+                        ? router.asPath === "/"
+                        : router.asPath.includes(item.href)) && isClient;
+                    return (
+                      <li
+                        key={`${item.label}-${i}`}
+                        className={`${
+                          activeItem ? activeItemClasses[theme.color] : ""
+                        }`}
+                      >
+                        <Link
+                          data-tina-field={tinaField(item, "label")}
+                          href={`/${item.href}`}
+                          className={`relative select-none	text-base inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-8 px-4 ${
+                            activeItem ? `` : `opacity-70`
                           }`}
-                          preserveAspectRatio="none"
-                          viewBox="0 0 230 230"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
                         >
-                          <rect
-                            x="230"
-                            y="230"
-                            width="230"
-                            height="230"
-                            transform="rotate(-180 230 230)"
-                            fill="url(#paint0_radial_1_33)"
-                          />
-                          <defs>
-                            <radialGradient
-                              id="paint0_radial_1_33"
-                              cx="0"
-                              cy="0"
-                              r="1"
-                              gradientUnits="userSpaceOnUse"
-                              gradientTransform="translate(345 230) rotate(90) scale(230 115)"
+                          {item.label}
+                          {activeItem && (
+                            <svg
+                              className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2 -z-1 opacity-10 dark:opacity-15 ${
+                                activeBackgroundClasses[theme.color]
+                              }`}
+                              preserveAspectRatio="none"
+                              viewBox="0 0 230 230"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
                             >
-                              <stop stopColor="currentColor" />
-                              <stop
-                                offset="1"
-                                stopColor="currentColor"
-                                stopOpacity="0"
+                              <rect
+                                x="230"
+                                y="230"
+                                width="230"
+                                height="230"
+                                transform="rotate(-180 230 230)"
+                                fill="url(#paint0_radial_1_33)"
                               />
-                            </radialGradient>
-                          </defs>
-                        </svg>
-                      )}
-                    </Link>
-                  </li>
-                );
-              })}
-          </ul>
+                              <defs>
+                                <radialGradient
+                                  id="paint0_radial_1_33"
+                                  cx="0"
+                                  cy="0"
+                                  r="1"
+                                  gradientUnits="userSpaceOnUse"
+                                  gradientTransform="translate(345 230) rotate(90) scale(230 115)"
+                                >
+                                  <stop stopColor="currentColor" />
+                                  <stop
+                                    offset="1"
+                                    stopColor="currentColor"
+                                    stopOpacity="0"
+                                  />
+                                </radialGradient>
+                              </defs>
+                            </svg>
+                          )}
+                        </Link>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
         </div>
         <div
           className={`absolute h-1 bg-gradient-to-r from-transparent ${
---
+++ components/layout/mobile-menu.tsx
@@ -0,0 +1,135 @@
+import { useRef, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useOverlayTriggerState } from "@react-stately/overlays";
+import {
+  useOverlay,
+  usePreventScroll,
+  useModal,
+  OverlayContainer,
+} from "@react-aria/overlays";
+import { useDialog } from "@react-aria/dialog";
+import { FocusScope } from "@react-aria/focus";
+import { useButton } from "@react-aria/button";
+
+import { GlobalHeader } from "../../tina/__generated__/types";
+
+function ModalDialog(
+  props: Parameters<typeof useOverlay>[0] &
+    Parameters<typeof useDialog>[0] & { data: GlobalHeader },
+) {
+  const router = useRouter();
+
+  const ref = useRef<HTMLElement | null>(null);
+  const { modalProps } = useModal();
+  const { overlayProps } = useOverlay(props, ref);
+  const { dialogProps } = useDialog(props, ref);
+
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  usePreventScroll();
+
+  return (
+    <div className="fixed z-[100] top-[72px] left-0 right-0 bottom-0 bg-white flex items-center justify-center">
+      <FocusScope contain restoreFocus autoFocus>
+        <nav
+          className="m-auto flex flex-col items-center"
+          {...overlayProps}
+          {...dialogProps}
+          {...modalProps}
+          ref={ref}
+        >
+          {props.data.nav &&
+            props.data.nav.map((item) => {
+              const activeItem =
+                (item.href === ""
+                  ? router.asPath === "/"
+                  : router.asPath.includes(item.href)) && isClient;
+
+              return (
+                <Link
+                  key={item.href}
+                  href={`/${item.href}`}
+                  className={`relative select-none text-lg inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-8 px-4 ${
+                    activeItem ? `` : `opacity-70`
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              );
+            })}
+        </nav>
+      </FocusScope>
+    </div>
+  );
+}
+
+export default function MobileMenu({ data }: { data: GlobalHeader }) {
+  const state = useOverlayTriggerState({});
+  const ref = useRef<HTMLButtonElement | null>(null);
+  const { buttonProps } = useButton(
+    {
+      onPress: () => (state.isOpen ? state.close() : state.open()),
+    },
+    ref,
+  );
+
+  return (
+    <div className="block sm:hidden">
+      <button
+        aria-label="Mobile Menu"
+        type="button"
+        className="border-0 cursor-pointer bg-transparent m-0"
+        {...buttonProps}
+        ref={ref}
+      >
+        {state.isOpen ? (
+          <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            fill="none"
+            shapeRendering="geometricPrecision"
+            className="text-black block"
+          >
+            <path d="M18 6L6 18" />
+            <path d="M6 6l12 12" />
+          </svg>
+        ) : (
+          <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            fill="none"
+            shapeRendering="geometricPrecision"
+            className="text-black block"
+          >
+            <path d="M3 12h18" />
+            <path d="M3 6h18" />
+            <path d="M3 18h18" />
+          </svg>
+        )}
+      </button>
+      {state.isOpen && (
+        <OverlayContainer>
+          <ModalDialog
+            isOpen
+            data={data}
+            onClose={(...props) => state.close(...props)}
+          />
+        </OverlayContainer>
+      )}
+    </div>
+  );
+}
