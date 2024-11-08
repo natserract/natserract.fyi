d30875cb22768c95567b674036a5f19fd209c050
natserract <benjaminstwo@gmail.com>
2024-11-08 23:52:22 +0800

    reconfigure site metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
@@ -7,15 +7,21 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-const site = "https://engineering-natserract.vercel.app";
+import vercel from "@astrojs/vercel/serverless";
+
+const site = "https://natserract-fyi.vercel.app";
 
 // https://astro.build/config
 export default defineConfig({
-  site,
-  integrations: [mdx(), sitemap(), tailwind(), react()],
-  markdown: {
-    shikiConfig: {
-      theme: "github-light",
+    site,
+    integrations: [mdx(), sitemap(), tailwind(), react()],
+
+    markdown: {
+        shikiConfig: {
+            theme: "github-light",
+        },
     },
-  },
+
+    output: "server",
+    adapter: vercel(),
 });
